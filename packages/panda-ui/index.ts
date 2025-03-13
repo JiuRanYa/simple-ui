123751fa284c84c80ce3ba5a75cf6132784243c5
JiuRanYa <1084350607@qq.com>
2025-03-13 16:25:38 +0800

    chore: rename project from Panda UI to Simple UI and update package details

--- packages/panda-ui/index.ts
+++
@@ -1,4 +0,0 @@
-export * from '@panda-ui/components'
-export * from '@panda-ui/hooks'
-export * from '@panda-ui/utils'
-export * from '@panda-ui/common'
---
+++ packages/simple-ui/index.ts
@@ -0,0 +1,4 @@
+export * from '@simple-ui/components'
+export * from '@simple-ui/hooks'
+export * from '@simple-ui/utils'
+export * from '@simple-ui/common'
